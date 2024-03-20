6476d38b5e337788dd7781905db3f010edecc3ac
b0jun <bjkim0228@naver.com>
2024-03-20 16:18:34 +0900

    feat: add example

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -3,7 +3,7 @@ export async function getPost() {
 	try {
 		// API Call
 		throw new Error('Email already exists', {
-			cause: { status: 400 },
+			cause: { status: 400, code: 'BA001' },
 		});
 	} catch (error) {
 		throw error;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,30 @@
 'use client';
 
 import { getPost } from '@/actions/actions';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
+	const [errorStatus, setErrorStatus] = useState();
+	const [errorCode, setErrorCode] = useState();
+
 	useEffect(() => {
 		const callApi = async () => {
 			try {
 				await getPost();
 			} catch (error: any) {
 				console.error(error.cause); // undefined
+				setErrorStatus(error.cause?.status);
+				setErrorCode(error.cause?.code);
 			}
 		};
 
 		callApi();
 	}, []);
-	return <div>Server Action Error Test</div>;
+	return (
+		<div>
+			<h3>Server Action Error Test</h3>
+			<p>Error Status: {errorStatus}</p>
+			<p>Error Code: {errorCode}</p>
+		</div>
+	);
 }
